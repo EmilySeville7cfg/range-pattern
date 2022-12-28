5e9244828e13f843ca92606f126473e66b5762a8
EmilySeville7cfg <EmilySeville7cfg@gmail.com>
2022-12-28 10:53:46 +1000

    Initial commit

---
+++ README.md
@@ -0,0 +1,72 @@
+# range-pattern
+
+## Requirenments
+
+- `emacs 27.1` higher
+
+## Features
+
+- [x] Function to check whether number is in a specific range
+- [x] `in` and `not-in` operators for `pcase`
+
+## Installation
+
+### Copy-paste plugin script contents to `~/.emacs` while enabling lexical-binding
+
+Example `~/.emacs` config:
+
+```emacs
+;;; -*- lexical-binding: t; -*-
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+(package-initialize)
+
+;; The extension file content with all comments removed can be placed here.
+
+(custom-set-variables
+;; custom-set-variables was added by Custom.
+;; If you edit it by hand, you could mess it up, so be careful.
+;; Your init file should contain only one such instance.
+;; If there is more than one, they won't work right.
+'(package-selected-packages '(markdown-mode)))
+(custom-set-faces
+;; custom-set-faces was added by Custom.
+;; If you edit it by hand, you could mess it up, so be careful.
+;; Your init file should contain only one such instance.
+;; If there is more than one, they won't work right.
+)
+```
+
+## Functionality
+
+By convention all parameters are ordered as follows:
+
+- `number`, `from`, `to`, `exclude-from`, and `exclude-from` (for function)
+- `from`, and `to` (for pattern in `pcase`)
+
+Below square brackets are used to represent optional parameters and **not** arrays.
+
+- Function: `range-pattern-check number from to [exclude-from exclude-to]`
+  Description: Check whether a number in a specific range.
+
+## Examples
+
+Check whether 20 in a `[10 50]` range:
+
+```lisp
+(range-pattern-check 20 10 50)
+```
+
+Check whether 20 in a `[10 50]` range via pattern matching:
+
+```lisp
+(pcase 20
+  ((in 10 50) "in range")
+  (_ "anything else"))
+
+```
+
+## FAQ
+
+- Why to use generators to generate sequences?
+  As long as sequences can be extremely long not to consume too much memory it's better to use generators.
---
+++ range-pattern.el
@@ -0,0 +1,70 @@
+;;; range-pattern.el --- Range pattern for pcase  -*- lexical-binding: t; -*-
+
+;; Copyright (c) 2022 Emily Grace Seville <EmilySeville7cfg@gmail.com>
+;; Version: 0.1
+;; Package-Requires: ((emacs "27.1"))
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+;;  For details view README file.
+
+;; URL: https://github.com/Emilyseville7cfg/generators
+
+;;; Code:
+
+(defun range-pattern-check(number from to &optional exclude-from exclude-to)
+  "Check whether a number in a specific range.
+FROM: the lowest range boundary
+TO: the highest range boundary
+EXCLUDE-FROM: whether to exclude the lowest range boundary
+EXCLUDE-TO: whether to exclude the highest range boundary"
+  (unless (integerp from)
+    (error "The lowest range boundary '%s' is not an integer" from))
+  (unless (integerp to)
+    (error "The highest range boundary '%s' is not an integer" to))
+  (unless (booleanp exclude-from)
+    (error "The lowest range boundary exclusion flag '%s' is not a boolean" exclude-from))
+  (unless (booleanp exclude-to)
+    (error "The highest range boundary exclusion flag '%s' is not a boolean" exclude-to))
+
+  (let ((from-checker (cond
+		       (exclude-from '(lambda (value from) (< from value)))
+		       (t '(lambda (value from) (<= from value)))))
+	(to-checker (cond
+		       (exclude-to '(lambda (value to) (> to value)))
+		       (t '(lambda (value to) (>= to value))))))
+    (and (funcall from-checker number from) (funcall to-checker number to))))
+
+(defmacro range-pattern--internal-wrapper(from to number)
+  (list 'range-pattern-check number from to))
+
+(defmacro range-pattern--negated-internal-wrapper(from to number)
+  (list 'not (list 'range-pattern-check number from to)))
+
+(pcase-defmacro in(from to)
+  "Check whether EXPVAL is in a specific range [from..to].
+FROM: the lowest range boundary
+TO: the highest range boundary"
+  `(pred (range-pattern--internal-wrapper ,from ,to)))
+
+(pcase-defmacro not-in(from to)
+  "Check whether EXPVAL is not in a specific range [from..to].
+FROM: the lowest range boundary
+TO: the highest range boundary"
+  `(pred (range-pattern--negated-internal-wrapper ,from ,to)))
+
+(provide 'range-pattern)
+
+;;; range-pattern.el ends here
